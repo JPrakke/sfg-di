6a6c60eb09289fb899f24eeff44dffd3b413f522
JoshPrakke <Joshua.Prakke@capco.com>
2022-04-14 22:16:33 -0400

    Add DI examples with spring

--- src/main/java/com/JPrakke/sfgdi/SfgDiApplication.java
+++ src/main/java/com/JPrakke/sfgdi/SfgDiApplication.java
@@ -1,6 +1,10 @@
 package com.JPrakke.sfgdi;
 
+import com.JPrakke.sfgdi.controllers.ConstructorInjectedController;
 import com.JPrakke.sfgdi.controllers.MyController;
+import com.JPrakke.sfgdi.controllers.PropertyInjectedController;
+import com.JPrakke.sfgdi.controllers.SetterInjectedController;
+import com.sun.source.tree.UsesTree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +20,24 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+
+		System.out.println("------ Property");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter");
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------ Constructor");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
--- src/main/java/com/JPrakke/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/JPrakke/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,10 +1,13 @@
 package com.JPrakke.sfgdi.controllers;
 
 import com.JPrakke.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
-
+//    autowired is not needed on constructors
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
